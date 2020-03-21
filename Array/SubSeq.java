c34b49735fb904b4a8788838f526dd396f45ca2c
prakash jha <prkjha25@gmail.com>
2020-03-21 23:00:27 +0530

    subsequence in an a array  a[i]<a[j]<a[k] & i<j<k

--- Array/SubSeq.java
+++ Array/SubSeq.java
@@ -1,72 +1,80 @@
-// { Driver Code Starts
-
-import java.util.*;
-import java.io.*;
-class SubSeq {
-    public static boolean isSubSequence(ArrayList<Integer> v1, ArrayList<Integer> v2,
-                                 int n, int m) {
-
-        if (m == 0) return true;
-        if (n == 0) return false;
-
-        if (v1.get(n - 1) == v2.get(m - 1))
-            return isSubSequence(v1, v2, n - 1, m - 1);
-
-        return isSubSequence(v1, v2, n - 1, m);
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-        while (tc-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] str = br.readLine().trim().split(" ");
-            ArrayList<Integer> a = new ArrayList<Integer>();
-            for (int i = 0; i < n; i++) {
-                a.add(Integer.parseInt(str[i]));
-            }
-            GfG g = new GfG();
-            ArrayList<Integer> res = g.find3Numbers(a, n);
-
-            // wrong format output
-            if (!res.isEmpty() && res.size() != 3) {
-                System.out.println(-1);
-            }
-
-            if (res.isEmpty()) {
-                System.out.println(0);
-            } else if ((res.get(0) < res.get(1)) && (res.get(1) < res.get(2)) &&
-                       isSubSequence(a, res, n, res.size())) {
-                System.out.println(1);
-            } else {
-                System.out.println(-1);
-            }
-        }
-    }
-}// } Driver Code Ends
-/*Complete the function below*/
-
-class GfG {
-    ArrayList<Integer> find3Numbers(ArrayList<Integer> arr, int n) {
-        ArrayList<Integer> l =  new ArrayList<Integer>();
-        // Add max values as placeholder for increasing pair.
-        l.add(Integer.MAX_VALUE); l.add(Integer.MAX_VALUE);
-        
-        // Look for better increasing pair as well as bigger 3rd element
-        for(int i=1; i<n; i++) {
-            if(arr.get(i-1)<arr.get(i) && arr.get(i)<l.get(1)) {
-                l.clear();
-                l.add(arr.get(i-1)); l.add(arr.get(i));
-            }
-            if (arr.get(i)>l.get(1)){
-                l.add(arr.get(i));
-                return l;
-            }
-        }
-        
-        // If we are here means we did not find 3 sorted elements.
-        // return empty list.
-        l.clear();
-        return l;
-    }
-}
+// Java program to find a sorted 
+// sub-sequence of size 3 
+import java.io.*; 
+  
+class SortedSubsequence { 
+    // A function to find a sorted 
+    // sub-sequence of size 3 
+    static void find3Numbers(int arr[]) 
+    { 
+        int n = arr.length; 
+  
+        // Index of maximum element 
+        // from right side 
+        int max = n - 1; 
+  
+        // Index of minimum element 
+        // from left side 
+        int min = 0; 
+        int i; 
+  
+        // Create an array that will store 
+        // index of a smaller element on left side. 
+        // If there is no smaller element on left 
+        // side, then smaller[i] will be -1. 
+        int[] smaller = new int[n]; 
+  
+        // first entry will always be -1 
+        smaller[0] = -1; 
+        for (i = 1; i < n; i++) { 
+            if (arr[i] <= arr[min]) { 
+                min = i; 
+                smaller[i] = -1; 
+            } 
+            else
+                smaller[i] = min; 
+        } 
+  
+        // Create another array that will 
+        // store index of a greater element 
+        // on right side. If there is no greater 
+        // element on right side, then greater[i] 
+        // will be -1. 
+        int[] greater = new int[n]; 
+  
+        // last entry will always be -1 
+        greater[n - 1] = -1; 
+        for (i = n - 2; i >= 0; i--) { 
+            if (arr[i] >= arr[max]) { 
+                max = i; 
+                greater[i] = -1; 
+            } 
+            else
+                greater[i] = max; 
+        } 
+  
+        // Now find a number which has 
+        // both greater number on right 
+        // side and smaller number on left side 
+        for (i = 0; i < n; i++) { 
+            if ( 
+                smaller[i] != -1 && greater[i] != -1) { 
+                System.out.print( 
+                    arr[smaller[i]] + " " + arr[i] 
+                    + " " + arr[greater[i]]); 
+                return; 
+            } 
+        } 
+  
+        // If we reach number, then there 
+        // are no such 3 numbers 
+        System.out.println("No such triplet found"); 
+        return; 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        int arr[] = { 12, 11, 10, 5, 6, 2, 30 }; 
+        find3Numbers(arr); 
+    } 
+} 
