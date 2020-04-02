e41dcdde7b4b85bed9379f75166b6e447b4b7dea
prakash jha <prkjha25@gmail.com>
2020-04-02 21:13:02 +0530

    merger sort java

---
+++ sorting/Merge_Sort.java
@@ -0,0 +1,91 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Merge_Sort
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    static void mergeSort(int arr[], int l, int r)
+    {
+        GfG g = new GfG();
+        if (l < r)
+        {
+            int m = (l+r)/2;
+            mergeSort(arr, l, m);
+            mergeSort(arr , m+1, r);
+            g.merge(arr, l, m, r);
+        }
+    }
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			Merge_Sort ms = new Merge_Sort();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			GfG g = new GfG();
+			mergeSort(arr,0,arr.length-1);
+
+			ms.printArray(arr);
+		T--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+/* The task is to complete merge() which is used
+in below mergeSort() */
+class GfG
+{
+   // Merges two subarrays of arr[].  First subarray is arr[l..m]
+   // Second subarray is arr[m+1..r]
+    void merge(int A[], int left, int middle, int right)
+    {
+      int [] leftTmpArray = new int[middle-left+2];  //Create tmp arrays
+		int [] rightTmpArray = new int[right-middle+1];
+		
+		for(int i=0;i<=middle-left;i++) //Copy values from Array 'A' to these tmp arrays
+			leftTmpArray[i]= A[left+i];
+		for(int i=0;i<right-middle;i++)
+			rightTmpArray[i]= A[middle+1+i];
+		
+		leftTmpArray[middle-left+1]= Integer.MAX_VALUE; //Merge values and insert into Array 'A'
+		rightTmpArray[right-middle] = Integer.MAX_VALUE;
+		int i = 0, j = 0;
+		for (int k = left; k <= right; k++) {
+			if (leftTmpArray[i] < rightTmpArray[j]) {
+				A[k] = leftTmpArray[i];
+				i++;
+			} else {
+				A[k] = rightTmpArray[j];
+				j++;
+			}
+		}
+    }
+}
+
+ /* This method is present in a class other than GfG class .
+static void mergeSort(int arr[], int l, int r)
+ {
+    GfG g = new GfG();
+    if (l < r)   
+   {
+        int m = (l+r)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr , m+1, r);
+        g.merge(arr, l, m, r);
+    }
+}*/
