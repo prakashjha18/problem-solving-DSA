8ddfbaf424ff1d5fb69ee21e91e110573759ece5
prakash jha <prkjha25@gmail.com>
2020-04-11 17:48:14 +0530

    validate IPv4 address

---
+++ strings/validip.java
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+  public class validip {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+
+            if (obj.isValidIP(s))
+                System.out.println(1);
+            else
+                System.out.println(0);
+        }
+    }
+}// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+
+    public boolean isValidIP(String s) {
+        return s.matches("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+    }
+}
