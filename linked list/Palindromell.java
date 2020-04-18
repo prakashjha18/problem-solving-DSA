0c90bc270e8b6113cd82d9450df08ec522d82781
prakash jha <prkjha25@gmail.com>
2020-04-18 23:53:50 +0530

    check if linked list is palindrome

---
+++ linked list/Palindromell.java
@@ -0,0 +1,128 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinkedList_Palindrom
+{
+	 Node head;  
+	
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+		} else 
+		{
+		   Node temp = head;
+		   while (temp.next != null)
+		   temp = temp.next;
+
+		   temp.next = node;
+		}
+    }
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		 
+		while(t>0)
+        {
+			int n = sc.nextInt();
+			//int k = sc.nextInt();
+			Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+			    tail.next = new Node(a);
+			    tail = tail.next;
+			}
+			
+			Palindrome g = new Palindrome();
+			if(g.isPalindrome(head) == true)
+			    System.out.println(1);
+		    else
+			    System.out.println(0);
+			t--;
+		}
+		
+	}
+}
+
+
+
+// } Driver Code Ends
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class Palindromell
+{
+    // Function to check if linked list is palindrome
+    boolean isPalindrome(Node head) 
+    {
+        Stack<Integer> stack = new Stack<Integer>();
+        Node fast=head;
+        Node slow=head;
+        int size=0;
+        while(fast!=null && fast.next!=null){
+            stack.push(slow.data);
+            slow=slow.next;
+            fast=fast.next.next;
+            size++;
+        }
+        if (fast != null) { 
+			slow = slow.next;
+		}
+		
+		while (slow != null) {
+			int top = stack.pop().intValue();
+			if (top != slow.data) {
+				return false;
+			}
+			slow = slow.next;
+		}
+		return true;
+        
+    }    
+}
+
