5891f6044d14235d3021eef91ebd0cd618102514
prakash jha <prkjha25@gmail.com>
2020-04-19 21:24:21 +0530

    remove duplicates from a linked list

---
+++ linked list/Remove_Duplicate_From_LL.java
@@ -0,0 +1,111 @@
+// { Driver Code Starts
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+class Remove_Duplicate_From_LL
+{
+    Node head;  
+
+	public void addToTheLast(Node node) 
+	{
+
+	  if (head == null) 
+	  {
+	    head = node;
+	  }
+	  else{
+	   Node temp = head;
+	   while (temp.next != null)
+		temp = temp.next;
+
+	   temp.next = node;
+	  }
+	}
+
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+       
+        
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		  
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL();
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+		//llist.printList();	
+        GfG g = new GfG();
+		llist.head = g.removeDuplicates(llist.head);
+		llist.printList();
+		
+        t--;		
+        }
+    }
+}
+// } Driver Code Ends
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+*/
+
+class GfG
+{
+    // Function to remove duplicates from the given linked list
+    public Node removeDuplicates(Node head) 
+    {
+         // Your code here
+        HashSet<Integer> hs = new HashSet<>(); 
+      
+        Node current = head; 
+        Node fd = head;
+        Node prev = null;
+        while(current!=null){
+            if(hs.contains(current.data))
+                prev.next=current.next;
+            else{
+                hs.add(current.data);
+                prev=current;
+            }
+            current=current.next;
+        }
+        return fd;
+    }
+}
