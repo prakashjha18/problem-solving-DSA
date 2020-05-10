c19840377c14b21fd9b05ba5369c3426cc6703fe
prakash jha <prkjha25@gmail.com>
2020-05-11 00:01:40 +0530

    Diagonal Sum In Binary Tree

---
+++ Trees/diagonalsum.java
@@ -0,0 +1,159 @@
+/*Consider Red lines of slope -1 passing between nodes (in following diagram). 
+The diagonal sum in a binary tree is the sum of all node’s data lying between these lines.
+Given a Binary Tree of size N, print all diagonal sums.
+
+For the following input tree, output should be 9, 19, 42.
+9 is sum of 1, 3 and 5.
+19 is sum of 2, 6, 4 and 7.
+42 is sum of 9, 10, 11 and 12.
+
+           1
+          / \
+         2   3
+        / \ / \
+       9  6 4  5
+        \ / /\
+        10 11 12 7
+*/
+
+// { Driver Code Starts
+
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class diagonalsum {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0)
+	        {
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+			    g.diagonalSum(root);
+			    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+class Tree {
+    public static void diagonalSum(Node root, int diagonal, Map<Integer, Integer> map)
+	{
+	    if(root == null){
+	        return;
+	    }
+	    map.put(diagonal,map.getOrDefault(diagonal,0)+root.data);
+	    diagonalSum(root.left, diagonal + 1, map);
+	    diagonalSum(root.right, diagonal, map);
+	}
+    public void diagonalSum(Node root) {
+        // add code here.
+        Map<Integer, Integer> map = new HashMap<>();
+        diagonalSum(root, 0, map);
+        for (int i = 0; i < map.size(); i++) {
+			System.out.print(map.get(i) + " ");
+		}
+    }
+}
