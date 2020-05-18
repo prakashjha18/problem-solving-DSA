e645f983555f6f13dce98955fb58ebb72e4336d9
prakash jha <prkjha25@gmail.com>
2020-05-18 14:51:37 +0530

    print all of a key ancestors in binary tree

---
+++ Trees/printancestors.java
@@ -0,0 +1,147 @@
+// { Driver Code Starts
+//Initial Template for Java
+// Given a Binary Tree and a target key, your task is to complete the function printAncestors() 
+// that prints all the ancestors of the key in the given binary tree.
+
+//               1
+//             /   \
+//           2      3
+//         /  \
+//       4     5
+//      /
+//     7
+// Key: 7
+// Ancestor: 4 2 1
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Main
+{
+    static Node buildTree(String str)
+    {
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static void main (String[] args) throws IOException
+    {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t=Integer.parseInt(br.readLine());
+        
+        while(t-- > 0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            int target=Integer.parseInt(br.readLine());
+            
+            GfG g = new GfG();
+            g.printAncestors(root, target);
+            System.out.println();
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+/*class Node of the binary tree
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}*/
+class GfG
+{
+    
+    public static boolean printAncestors(Node root, int x)
+    {
+        // add your code here
+        if(root==null){
+        return false;
+        }
+        if(root.data == x){
+        return true;
+        }
+        if(printAncestors(root.left,x)||printAncestors(root.right,x)){
+            System.out.print(root.data+" ");
+            return true;
+        }
+        
+        return false;
+        
+    }
+}
