afe867ecb62716154e8c45561aff15443e55afeb
prakash jha <prkjha25@gmail.com>
2020-06-06 00:54:34 +0530

    box stacking

--- dynamic programming/boxstacking.java
+++ dynamic programming/boxstacking.java
@@ -43,62 +43,114 @@ class gfg
 
 
 
-class Box{
-    int h;
-    int w;
-    int d;
-    int area;
-    
-    public Box(int h,int w,int d){
-        this.h = h;
-        this.w = w;
-        this.d = d;
-        area = w * d;
+class Dimension implements Comparable<Dimension> {
+    int height;
+    int length;
+    int width;
+
+    Dimension(int height, int length, int width) {
+        this.height = height;
+        this.length = length;
+        this.width = width;
+    }
+
+    Dimension() {
+    }
+
+    static Dimension createDimension(int height, int side1, int side2) {
+        Dimension d = new Dimension();
+        d.height = height;
+        if (side1 >= side2) {
+            d.length = side1;
+            d.width = side2;
+        } else {
+            d.length = side2;
+            d.width = side1;
+        }
+        return d;
+        
+    }
+
+    @Override
+    public int compareTo(Dimension d) {
+        if (this.length * this.width >= d.length * d.width) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Dimension [height=" + height + ", length=" + length
+                + ", width=" + width + "]";
     }
 }
 class Geeks 
-{
+{   public int maxHeight(Dimension[] input) {
+        //get all rotations of box dimension.
+        //e.g if dimension is 1,2,3 rotations will be 2,1,3  3,2,1  3,1,2  . Here length is always greater
+        //or equal to width and we can do that without loss of generality.
+        Dimension[] allRotationInput = new Dimension[input.length * 3];
+        createAllRotation(input, allRotationInput);
+        
+        //sort these boxes in non increasing order by their base area.(length X width)
+        Arrays.sort(allRotationInput);
+
+        //apply longest increasing subsequence kind of algorithm on these sorted boxes.
+        int T[] = new int[allRotationInput.length];
+
+        for (int i = 0; i < T.length; i++) {
+            T[i] = allRotationInput[i].height;
+        }
+
+        for (int i = 1; i < T.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (allRotationInput[i].length < allRotationInput[j].length
+                        && allRotationInput[i].width < allRotationInput[j].width) {
+                    if( T[j] + allRotationInput[i].height > T[i]){
+                        T[i] = T[j] + allRotationInput[i].height;
+                    }
+                }
+            }
+        }
+       
+        //find max in T[] and that will be our max height.
+        //Result can also be found using result[] array.
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i < T.length; i++){
+            if(T[i] > max){
+                max = T[i];
+            }
+        }
+        
+        return max;
+    }
+
+    //create all rotations of boxes, always keeping length greater or equal to width
+    private void createAllRotation(Dimension[] input,
+            Dimension[] allRotationInput) {
+        int index = 0;
+        for (int i = 0; i < input.length; i++) {
+            allRotationInput[index++] = Dimension.createDimension(
+                    input[i].height, input[i].length, input[i].width);
+            allRotationInput[index++] = Dimension.createDimension(
+                    input[i].length, input[i].height, input[i].width);
+            allRotationInput[index++] = Dimension.createDimension(
+                    input[i].width, input[i].length, input[i].height);
+
+        }
+    }
     public static int maxHeight(int height[], int width[], int length[], int n)
-   {
-       Comparator<Box> comp = (b1,b2) -> -Integer.compare(b1.area,b2.area);
-       
-       Box box[] = new Box[n];
-       for(int i = 0;i<n;i++){
-           box[i] = new Box(height[i],width[i],length[i]);
-       }
-       
-       Box allRotationsArr[] = new Box[3 * n];
-       
-       int index = -1;
-       for(int i = 0;i<n;i++){
-           allRotationsArr[++index] = new Box(box[i].h,Math.max(box[i].d,box[i].w),Math.min(box[i].d,box[i].w));
-           allRotationsArr[++index] = new Box(box[i].w,Math.max(box[i].d,box[i].h),Math.min(box[i].d,box[i].h));
-           allRotationsArr[++index] = new Box(box[i].d,Math.max(box[i].w,box[i].h),Math.min(box[i].w,box[i].h));
-
-       }
-
-       Arrays.sort(allRotationsArr,comp);
-       
-       int newLength = 3 * n;
-       int msh[] = new int[newLength];
-       
-       for(int i = 0;i<newLength;i++){
-           msh[i] = allRotationsArr[i].h;
-       }
-       
-       
-       int max = -1;
-       for(int i = 1;i<newLength;i++){
-           for(int j = 0;j<i;j++){
-                if(allRotationsArr[i].d < allRotationsArr[j].d && allRotationsArr[i].w < allRotationsArr[j].w)
-                    msh[i] = Math.max(msh[i],msh[j] + allRotationsArr[i].h);
-           }
-           
-           max = Math.max(max,msh[i]);
-       }
-
-       return max;
-   }
+    {
+        Geeks bs = new Geeks();
+        Dimension input[] = new Dimension[n];
+        for(int i = 0;i<n;i++){
+            input[i] = new Dimension(height[i],width[i],length[i]);
+        }
+        int maxHeight = bs.maxHeight(input);
+        return maxHeight;
+    }
 }
 
 
