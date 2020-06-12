5da5f753365efaac8057dfed78ba89bd50d6cd80
prakash jha <prkjha25@gmail.com>
2020-06-12 19:12:47 +0530

    Smallest window that contains all chrs of str itself

--- strings/samllestwindow.java
+++ strings/samllestwindow.java
@@ -1,5 +1,25 @@
 // Java program to find the smallest window containing 
 // all characters of a pattern. 
+// Given a string, find the smallest window length with all distinct characters of the given string. For eg. str = “aabcbcdbca”, then the result would be 4 as of the smallest window will be “dbca” .
+
+// Examples:
+
+// Input: aabcbcdbca
+// Output: dbca
+// Explanation: 
+// Possible substrings= {aabcbcd, abcbcd, 
+// bcdbca, dbca....}
+// Of the set of possible substrings 'dbca' 
+// is the shortest substring having all the 
+// distinct characters of given string. 
+
+// Input: aaab
+// Output: ab
+// Explanation: 
+// Possible substrings={aaab, aab, ab}
+// Of the set of possible substrings 'ab' 
+// is the shortest substring having all 
+// the distinct characters of given string.    
 import java.util.Arrays; 
 public class GFG { 
   
