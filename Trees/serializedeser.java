3fad877e90734e97b77d9b7ef08d741a064db2da
prakash jha <prkjha25@gmail.com>
2020-06-18 04:39:57 +0530

    serialize and deserialize the tree

--- Trees/serializedeser.java
+++ Trees/serializedeser.java
@@ -190,3 +190,48 @@ class Tree {
     }
     
 }
+
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        Queue<TreeNode> q = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                res.append("n ");
+                continue;
+            }
+            res.append(node.val + " ");
+            q.add(node.left);
+            q.add(node.right);
+        }
+        //System.out.print(res.toString()+"PPPP");
+        return res.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data == "") return null;
+        //System.out.print(data+"PPPP");
+        Queue<TreeNode> q = new LinkedList<>();
+        String[] values = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        q.add(root);
+        for (int i = 1; i < values.length; i++) {
+            TreeNode parent = q.poll();
+            if (!values[i].equals("n")) {
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                parent.left = left;
+                q.add(left);
+            }
+            if (!values[++i].equals("n")) {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                parent.right = right;
+                q.add(right);
+            }
+        }
+        return root;
+    }
+}
