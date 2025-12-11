dbcc4cc2e0eeead6426b720551ae796941ae8a3c
prakashjha18 <prkjha25@gmail.com>
2025-12-11 23:53:53 +0530

    added hashmap

--- hashing/twosum.java
+++ hashing/twosum.java
@@ -76,3 +76,19 @@ class twosum {
         return false;
     }
 }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i =0;i<nums.length; i++){
+            map.put(nums[i],i);
+        }
+        for(int i =0;i<nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && i != map.get(complement)){
+                return new int[] {i, map.get(complement)};
+            }
+        }
+        return new int[] {};
+    }
+}
