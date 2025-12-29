c18aab8d86526e44cef9d9558c20d5f1a465f0a8
prakashjha18 <prkjha25@gmail.com>
2025-12-29 17:28:11 +0530

    added 2nd apprahc fro meeting room2

--- Intervals/MeetingRoomsII.java
+++ Intervals/MeetingRoomsII.java
@@ -18,6 +18,7 @@
 // Input: intervals = [[7,10],[2,4]]
 // Output: 1
  
+//Priority queue solution
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
@@ -29,5 +30,36 @@ class Solution {
             pq.offer(intervals[i][1]);
         }
         return pq.size();
+    }
+}
+
+//two pointer solution
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int[] start = new  int[intervals.length];
+        int[] end = new int[intervals.length];
+        for(int i =0;i<intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int res = 0, count =0;
+        int s =0,e =0;
+
+        while(s<intervals.length){
+            if(start[s] < end[e]){
+                s++;
+                count=count + 1;
+            } else {
+                e++;
+                count=count - 1;
+            }
+            res = Math.max(res,count);
+        }
+        return res;
+       
     }
 }
