09514187c9885d9aab0addfeb323b225075836f6
prakashjha18 <prkjha25@gmail.com>
2025-12-30 23:40:20 +0530

    updated

--- linked list/ReorderList.java
+++ linked list/ReorderList.java
@@ -1,131 +1,78 @@
 //Given a singly linked list: A0→A1→…→An-1→An, reorder it to: A0→An→A1→An-1→A2→An-2→…
 //For example: Given 1->2->3->4->5 its reorder is 1->5->2->4->3.
-// { Driver Code Starts
-import java.util.*;
 
-class Node {
-    int data;
-    Node next;
-    Node(int d) {
-        data = d;
-        next = null;
-    }
-}
+// Reorder List
+// Solved
+// Medium
+// Topics
+// conpanies icon
+// Companies
+// You are given the head of a singly linked-list. The list can be represented as:
 
-  public class ReorderList {
-    Node head; // head of lisl
-    Node last; // last of linked list
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
 
-    /* Linked list Node*/
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 
-    /* Utility functions */
-
-    /* Inserts a new Node at front of the list. */
-    public void addToTheLast(Node node) {
-
-        if (head == null) {
-            head = node;
-            last = head;
-        } else {
-            // Node temp = head;
-            // while (temp.next != null) temp = temp.next;
-
-            last.next = node;
-            last = last.next;
-        }
-    }
-    /* Function to print linked list */
-    void printList() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-        System.out.println();
-    }
-
-    /* Drier program to test above functions */
-    public static void main(String args[]) {
-
-        /* Constructed Linked List is 1->2->3->4->5->6->
-           7->8->8->9->null */
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            int n = sc.nextInt();
-            ReorderList llist = new ReorderList();
-
-            int a1 = sc.nextInt();
-            Node head = new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) {
-                int a = sc.nextInt();
-                llist.addToTheLast(new Node(a));
-            }
-
-            llist.head = new gfg().reorderlist(llist.head);
-
-            llist.printList();
-
-            t--;
-        }
-    }
-}// } Driver Code Ends
-/* Following is the Linked list node structure */
-
-/*class Node
-{
-    int data;
-    Node next;
-    Node(int d) {
-        data = d;
-        next = null;
-    }
-}*/
-
-class gfg {
-    Node reverselist(Node node){ 
-        Node prev = null;
-        Node curr = node;
-        Node temp = node;
-        while(curr!=null){
-            temp = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=temp;
-            
-        }
-        return prev;
-    } 
-    Node reorderlist(Node node) {
-       Node slow = node, fast = slow.next; 
-        while (fast != null && fast.next != null) { 
-            slow = slow.next; 
-            fast = fast.next.next; 
-        } 
  
-        Node node1 = node; 
-        Node node2 = slow.next; 
-        slow.next = null; 
-  
-        node2 = reverselist(node2); 
-  
-        node = new Node(0); 
-        Node curr = node; 
-        while (node1 != null || node2 != null) { 
-            if (node1 != null) { 
-                curr.next = node1; 
-                curr = curr.next; 
-                node1 = node1.next; 
-            } 
-            if (node2 != null) { 
-                curr.next = node2; 
-                curr = curr.next; 
-                node2 = node2.next; 
-            } 
-        } 
-  
-        node = node.next; 
-        return node;
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4]
+// Output: [1,4,2,3]
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [1,5,2,4,3]
+ 
+import java.util.*;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+         if (head == null) {
+            return;
+        }
+        ListNode slow = head;
+        ListNode fast =head;
+        while(fast!=null && fast.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reverse
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = slow;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev=curr;
+            curr = next;
+        }
+
+        //now merge
+        ListNode first = head;
+        ListNode second = prev;
+        while(second.next != null){
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            first = temp1;
+            second.next = first;
+            second = temp2;
+        }
     }
 }
